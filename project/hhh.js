bde56aa5e97ff282bc328c18dfd7929a25cf1ca2
pradeep23-dot <pradeepbose555@gmail.com>
2026-06-21 23:36:15 +0530

    lol

--- project/hhh.js
+++
@@ -1,6 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/posts")
-{
-    method: "DELETE"
-}
-.then(response => response.json())
-.then(data => console.log(data))
--- project/webscript.js
+++ project/webscript.js
@@ -1,3 +1,25 @@
-console.log("HELLO")
-const first = document.getElementById("p1")
-first.innerHTML = "hello"
+function loadUsers()
+{
+    document.getElementById("status").innerHTML = "Loading...";
+
+    setTimeout(function()
+    {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(data =>
+        {
+            let output = "";
+
+            for(let i = 0; i < data.length; i++)
+            {
+                output += "Name: " + data[i].name + "<br>";
+                output += "Email: " + data[i].email + "<br>";
+                output += "Phone: " + data[i].phone + "<br><br>";
+            }
+
+            document.getElementById("users").innerHTML = output;
+
+            document.getElementById("status").innerHTML = "Loaded successfully";
+        });
+    }, 2000);
+}
